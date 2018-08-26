5d721df060c7280a5a43ec3e6619b4177204740f
Peter Musonye <musonyengigi@gmail.com>
2018-08-26 17:07:26 +0300

    chore: Add RidesController tests

--- spec/factories/ride.rb
+++ spec/factories/ride.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :ride do
+    vehicle
     origin { Faker::GameOfThrones.city }
     destination { Faker::GameOfThrones.city }
     capacity { Faker::Number.between(1, 9) }
     departure_time { Faker::Time.forward(23, :morning) }
-    vehicle
+    vehicle_id { nil }
   end
 end
